54326322fd6fa8a3c0cc0ad71d5b4a4368c054d5
Bryan Pereira Ferdinandi <bryan.ferdinandi@estudante.ufscar.br>
2024-01-30 16:26:51 -0300

    Update contagem_de_pixels.R
    Script de contagem de pixels por zona.

--- scripts/contagem_de_pixels.R
+++ scripts/contagem_de_pixels.R
@@ -1,11 +1,41 @@
-#Limpeza do ambiente
+# Limpeza do ambiente
 rm(list=ls())
 
-#carregar os pacotes
-library(raster)
-library(sf)
-library(dplyr)
+# Carregando bilbiotecas
+library(pacman)
+p_load(data.table, raster, rgdal, sf, fasterize, dplyr,exactextractr,tidyr)
 
+# Carregando rasters
+setwd("D:/GIT_WORKSPACE/conectividade-no-campo/dados/cleaned")
+values <- raster("clip_demanda_v5_albers_clip.tif")
+zones<-raster("mun_conectividade_albers.tif")
 
-#caregar arquivos necessário
-demanda <- raster('caminho/do/arquivo.tif')
+# Contagem dos pixels
+bss <- blockSize(values, minrows = 78)
+bss$n
+value <- NA
+area <- NA
+zone <- NA
+z <- data.frame(zone = zone, value = value, area = area)
+for(i in 1:bss$n) {
+  print(i)
+  x <- data.frame(zone = getValues(zones,  row = bss$row[i], nrows = bss$nrows[i]),
+                  value = getValues(values,  row = bss$row[i], nrows = bss$nrows[i]))
+  x <- drop_na(x)
+  y <- x %>% group_by(zone, value)%>%summarise(area = n()*1)  
+  z <- rbind(z,y)
+  z <- z%>%group_by(zone, value)%>%summarise(area = sum(area, na.rm = T))
+}
+
+#Tratamento
+z<-z%>%rename(
+  demanda=value,
+  area_km2=area,
+  cd_mun=zone
+)
+
+table_conec <- z[-c(1:10, 80),]
+
+# Exportar tabela
+write.table(table_conec, "D:/GIT_WORKSPACE/conectividade-no-campo/dados/cleaned/demanda_conectividade_mun.csv",
+             row.names = F, sep = ";", dec = ",")
